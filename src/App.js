2793dae4477a98d8af4ccf1fb92d384d97171d99
Justinian074 <60757343+Justinian074@users.noreply.github.com>
2020-10-23 14:07:53 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 import "./styles.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBuilding } from "@fortawesome/free-solid-svg-icons";
@@ -11,121 +12,229 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      individualTrue: false,
       loading: true,
       result: [],
       aggregations: [],
-      searchTerm: ""
+      searchTerm: "",
+      pageNumber: 0,
+      individualID: "",
+      affiliations: "",
+      resourceGroup: "",
+      type: ""
     };
   }
 
   handleSearchTerm = (event) => {
     this.setState({
-      searchTerm: event.target.value
+      searchTerm: event.target.value,
+      pageNumber: 0
     });
+  };
+
+  IncrementItem = () => {
+    this.setState({ pageNumber: this.state.pageNumber + 1 });
+    console.log(this.state.pageNumber);
+    this.componentDidMount();
+  };
+
+  DecreaseItem = () => {
+    this.setState({ pageNumber: this.state.pageNumber - 1 });
+    console.log(this.state.pageNumber);
+    this.componentDidMount();
+  };
+
+  SingleItem = () => {
+    this.setState({ individualTrue: true });
+    console.log("Hi");
+    console.log(this.props);
+    this.componentDidMount();
   };
 
   async componentDidMount() {
     //https://info.xsede.org/wh1/resource-api/v3/resource_esearch/?resource_groups=Software&page=1
     //https://info.xsede.org/wh1/resource-api/v3/resource_esearch/?resource_groups=Software&page=1&format=json
-    const url =
-      "https://info.xsede.org/wh1/resource-api/v3/resource_esearch/?format=json&aggregations=type,affiliation,resourcegroup&search_terms=" +
-      this.state.searchTerm;
+    if (this.state.individualTrue === false) {
+      const url =
+        "https://info.xsede.org/wh1/resource-api/v3/resource_esearch/?format=json&aggregations=type,affiliation,resourcegroup&search_terms=" +
+        this.state.searchTerm +
+        "&page=" +
+        this.state.pageNumber;
 
-    const response = await fetch(url);
-    const data = await response.json();
-    this.setState({
-      result: data.results,
-      aggregations: data.aggregations,
-      total: data.total_results,
-      loading: false
-    });
+      const response = await fetch(url);
+      const data = await response.json();
+      this.setState({
+        result: data.results,
+        aggregations: data.aggregations,
+        total: data.total_results,
+        loading: false
+      });
+    } else if (this.state.individualTrue === true) {
+      //const { id } = this.props.id;
+      console.log(this.props);
+      const url =
+        "https://info.xsede.org/wh1/resource-api/v3/resource/id/" +
+        //{id} +
+        "urn:ogf:glue2:uiuc.edu:resource:89616" +
+        //this.state.individualID +
+        "/?format=json";
+
+      const response = await fetch(url);
+      const data = await response.json();
+      this.setState({
+        result: data.results,
+        loading: false
+      });
+    }
   }
 
   render() {
-    const { loading, result, aggregations, total } = this.state;
-    if (loading) return <div>Loading...</div>;
-
-    return (
-      <Container fluid>
-        <div class="App" className="page">
-          <div className="archive">
-            <ul>
-              <h1>Resource List</h1>
-              <input
-                type="text"
-                value={this.state.searchTerm}
-                onChange={this.handleSearchTerm}
-              />
-              <button onClick={() => this.componentDidMount()}>Search</button>
-              <Row>
-                <Col xs={2}>
-                  <b>Affiliation</b> <br />
-                  Any: {total} <br />
-                  {aggregations.Affiliation[0].Name}:{" "}
-                  {aggregations.Affiliation[0].count} <br />
-                  {aggregations.Affiliation[1].Name}:{" "}
-                  {aggregations.Affiliation[1].count} <br />
-                  <b>Resource Group</b> <br />
-                  {aggregations.ResourceGroup[0].Name}:{" "}
-                  {aggregations.ResourceGroup[0].count} <br />
-                  {aggregations.ResourceGroup[1].Name}:{" "}
-                  {aggregations.ResourceGroup[1].count} <br />
-                  {aggregations.ResourceGroup[2].Name}:{" "}
-                  {aggregations.ResourceGroup[2].count} <br />
-                  {aggregations.ResourceGroup[3].Name}:{" "}
-                  {aggregations.ResourceGroup[3].count} <br />
-                  {aggregations.ResourceGroup[4].Name}:{" "}
-                  {aggregations.ResourceGroup[4].count} <br />
-                  {aggregations.ResourceGroup[5].Name}:{" "}
-                  {aggregations.ResourceGroup[5].count} <br />
-                  {aggregations.ResourceGroup[6].Name}:{" "}
-                  {aggregations.ResourceGroup[6].count} <br />
-                  <b>Type</b> <br />
-                  {aggregations.Type[0].Name}: {aggregations.Type[0].count}{" "}
-                  <br />
-                  {aggregations.Type[1].Name}: {aggregations.Type[1].count}{" "}
-                  <br />
-                  {aggregations.Type[2].Name}: {aggregations.Type[2].count}{" "}
-                  <br />
-                  {aggregations.Type[3].Name}: {aggregations.Type[3].count}{" "}
-                  <br />
-                  {aggregations.Type[4].Name}: {aggregations.Type[4].count}{" "}
-                  <br />
-                  {aggregations.Type[5].Name}: {aggregations.Type[5].count}{" "}
-                  <br />
-                  {aggregations.Type[6].Name}: {aggregations.Type[6].count}{" "}
-                  <br />
-                  {aggregations.Type[7].Name}: {aggregations.Type[7].count}{" "}
-                  <br />
-                  {aggregations.Type[8].Name}: {aggregations.Type[8].count}{" "}
-                  <br />
-                  {aggregations.Type[9].Name}: {aggregations.Type[9].count}{" "}
-                  <br />
-                </Col>
-                <Col xs={10}>
-                  {result.map((resource) => (
-                    <div key={resource.ID} class="article">
-                      <article class="article">
-                        Name: {resource.Name}
-                        <br />
-                        Description: {resource.Description}
-                        <br />
-                        <FontAwesomeIcon size="1x" icon={faBuilding} />
-                        {"  " + resource.Affiliation + "  "}
-                        <FontAwesomeIcon size="1x" icon={faArchive} />
-                        {"  " + resource.ResourceGroup + "  "}
-                        <FontAwesomeIcon size="1x" icon={faDatabase} />
-                        {"  " + resource.Type + "  "}
-                      </article>
-                    </div>
-                  ))}
-                </Col>
-              </Row>
-            </ul>
-          </div>
+    const { loading, result, aggregations, total, individualTrue } = this.state;
+    if (loading)
+      return (
+        <div>
+          <center>Loading...</center>
         </div>
-      </Container>
-    );
+      );
+
+    if (individualTrue === false) {
+      return (
+        <Router>
+          <React.Fragment>
+            <Container fluid>
+              <div class="App" className="page">
+                <div className="archive">
+                  <ul>
+                    <h1>Resource List</h1>
+                    <input
+                      type="text"
+                      value={this.state.searchTerm}
+                      onChange={this.handleSearchTerm}
+                    />
+                    <button onClick={() => this.componentDidMount()}>
+                      Search
+                    </button>
+                    <button onClick={this.DecreaseItem}>Prev Page</button>
+                    <button onClick={this.IncrementItem}>Next Page</button>
+                    <Row>
+                      <Col xs={2}>
+                        <b>Affiliation</b> <br />
+                        Any: {total} <br />
+                        {aggregations.Affiliation.map((affiliation) => (
+                          <div>
+                            {affiliation.Name}
+                            {": \n"}
+                            {affiliation.count}
+                          </div>
+                        ))}
+                        <b>Resource Group</b> <br />
+                        {aggregations.ResourceGroup.map((ResourceGroup) => (
+                          <div>
+                            {ResourceGroup.Name}
+                            {": \n"}
+                            {ResourceGroup.count}
+                          </div>
+                        ))}
+                        <b>Type</b> <br />
+                        {aggregations.Type.map((Type) => (
+                          <div>
+                            {Type.Name}
+                            {": \n"}
+                            {Type.count}
+                          </div>
+                        ))}
+                      </Col>
+                      <Col xs={10}>
+                        {result.map((resource) => (
+                          <div key={resource.ID} class="article">
+                            <article class="article">
+                              Name:{" "}
+                              <button class="link" onClick={this.SingleItem}>
+                                <Link to={"/" + resource.ID}>
+                                  {resource.Name}
+                                </Link>
+                              </button>
+                              <Route path="/:id" />
+                              <br />
+                              Description:{" "}
+                              <div
+                                dangerouslySetInnerHTML={{
+                                  __html: resource.Description
+                                }}
+                              ></div>
+                              <br />
+                              <FontAwesomeIcon size="1x" icon={faBuilding} />
+                              {"  " + resource.Affiliation + "  "}
+                              <FontAwesomeIcon size="1x" icon={faArchive} />
+                              {"  " + resource.ResourceGroup + "  "}
+                              <FontAwesomeIcon size="1x" icon={faDatabase} />
+                              {"  " + resource.Type + "  "}
+                            </article>
+                          </div>
+                        ))}
+                      </Col>
+                    </Row>
+                  </ul>
+                </div>
+              </div>
+            </Container>
+          </React.Fragment>
+        </Router>
+      );
+    }
+    if (individualTrue === true) {
+      return (
+        <Container fluid>
+          <div class="App" className="page">
+            <div className="archive">
+              <ul>
+                <h1>Resource List</h1>
+                <input
+                  type="text"
+                  value={this.state.searchTerm}
+                  onChange={this.handleSearchTerm}
+                />
+                <button onClick={() => this.componentDidMount()}>Search</button>
+                <button onClick={this.DecreaseItem}>Prev Page</button>
+                <button onClick={this.IncrementItem}>Next Page</button>
+                <Row>
+                  <Col xs={10}>
+                    {result.map((resource) => (
+                      <div key={resource.ID} class="article">
+                        <article class="article">
+                          <b>{resource.Name}</b>
+                          <br />
+                          Affiliation: {resource.Affiliation}
+                          <br />
+                          Resource Group: {resource.ResourceGroup}
+                          <br />
+                          Type: {resource.Type}
+                          <br />
+                          Description:
+                          <div
+                            dangerouslySetInnerHTML={{
+                              __html: resource.Description
+                            }}
+                          ></div>
+                          <br />
+                          <br />
+                          {resource.Relations &&
+                            resource.Relations.map((resource) => (
+                              <div>
+                                {resource.RelationType} {resource.Name}
+                              </div>
+                            ))}
+                        </article>
+                      </div>
+                    ))}
+                  </Col>
+                </Row>
+              </ul>
+            </div>
+          </div>
+        </Container>
+      );
+    }
   }
 }
 
